2ccc6467267c1ea459572762781238ec3273a24d
KBadia <KshirodKumar.Badia@microfocus.com>
2022-12-03 22:12:30 +0530

    test

--- src/test/java/com/blackboard/testing/tests/LambdaTest.java
+++ src/test/java/com/blackboard/testing/tests/LambdaTest.java
@@ -1,11 +1,13 @@
 package com.blackboard.testing.tests;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.title;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.assertThat;
+import static com.blackboard.testing.lambda.logger.LoggerContainer.LOGGER;
+import static io.restassured.RestAssured.given;
 
 import com.blackboard.testing.common.LambdaBaseTest;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -13,17 +15,21 @@ import org.junit.experimental.categories.Category;
 public class LambdaTest extends LambdaBaseTest {
 
     @Test
-    public void googleTest() {
-        open("http://www.google.com/");
-        screenshot("google-home-page");
-        assertThat(title(), containsString("Google"));
-
+    public void getWeatherDetails() {
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.request(Method.GET, "https://demoqa.com/BookStore/v1/Books");
+        String responseBody = response.getStatusLine();
+        LOGGER.log("Status Line is " + responseBody);
     }
 
     @Test
-    public void blackboardTest() {
-        open("http://www.blackboard.com/");
-        screenshot("blackboard-home-page");
-        assertThat(title(), containsString("Blackboard"));
+    public void getBookDetails() {
+        given()
+                .when()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK");
     }
 }
