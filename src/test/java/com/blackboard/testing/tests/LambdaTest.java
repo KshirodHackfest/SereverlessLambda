b49233410f3e0463d360d5b54233196017d6a96f
SVPillai <shruthi.vpillai@microfocus.com>
2022-12-04 15:01:48 +0530

    Demo test

--- src/test/java/com/blackboard/testing/tests/LambdaTest.java
+++ src/test/java/com/blackboard/testing/tests/LambdaTest.java
@@ -20,7 +20,6 @@ public class LambdaTest extends LambdaBaseTest {
         RequestSpecification httpRequest = RestAssured.given();
         Response response = httpRequest.request(Method.GET, "https://demoqa.com/BookStore/v1/Books");
         String responseBody = response.getStatusLine();
-        LOGGER.log("Status Line is :" + responseBody);
     }
 
     @Test
